16780875f31c53308883064a7b845ac69bbef85c
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2022-08-20 22:05:31 +0300

    public-private presets

--- web/IWHub/presets/urls.py
+++ web/IWHub/presets/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from django.http import HttpResponse
 
 from presets.views import upload_preset, DetailPresetView, DeletePresetView, EditPresetView
-from presets.views import ListAllPresetsView
+from presets.views import ListAllPresetsView, ListPrivatePresetsView, ListPublicPresetsView,ListPresetsView
 
 
 def test(request):
@@ -11,7 +11,10 @@ def test(request):
 
 
 urlpatterns = [
-    path('', ListAllPresetsView.as_view(), name="list"),
+    path('', ListPresetsView.as_view(), name="list"),
+    path('library/', ListPublicPresetsView.as_view(), name="library_presets"),
+    path('private/', ListPrivatePresetsView.as_view(), name="private"),
+
     path('<int:pk>', DetailPresetView.as_view(), name="detail"),
     path('edit/<int:pk>', EditPresetView.as_view(), name="edit"),
     path('delete/<int:pk>', DeletePresetView.as_view(), name="delete"),
@@ -20,3 +23,4 @@ urlpatterns = [
     # path('create/', PostCreateView.as_view(), name="create"),
 
 ]
+
--- web/IWHub/presets/views.py
+++ web/IWHub/presets/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, UpdateView, DeleteView, CreateView
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth import logout, authenticate, login
+from django.db.models import Q
 
 from django.contrib.auth.models import User
 from presets.models import Preset
@@ -13,21 +14,24 @@ import tarfile
 def upload_preset(request):
     FILE_IMAGE_NAME = "sys_wall.jpeg"
 
+
     if request.method == 'POST':
         archive = request.FILES.get('archive')
         title = request.POST['title']
         description = request.POST['description']
         widget_set = request.POST['widget_set']
-        private = True if request.POST['private'] == 'on' else False
+        # private = True if request.POST['private'] == 'on' else False
+        private = True if request.POST.get('private') else False
+
         author = request.user
 
         preset = Preset(archive=archive, title=title, description=description,
                         widget_set=widget_set, private=private, author=author)
-        preset.image = f"static/presets/{preset.uu_id}/{FILE_IMAGE_NAME}"
+        preset.image = f"storage/presets/{preset.uu_id}/{FILE_IMAGE_NAME}"
         preset.save()
 
-        with tarfile.open(f"static\presets\{preset.uu_id}\{archive}", 'r') as tar:
-            tar.extract(FILE_IMAGE_NAME, f"static\presets\{preset.uu_id}")
+        with tarfile.open(f"storage\presets\{preset.uu_id}\{archive}", 'r') as tar:
+            tar.extract(FILE_IMAGE_NAME, f"storage\presets\{preset.uu_id}")
 
         return HttpResponseRedirect('/')
 
@@ -38,6 +42,45 @@ class ListAllPresetsView(ListView): ###TEST
     model = Preset
     template_name = "main.html"
     context_object_name = "presets"
+
+
+class ListPublicPresetsView(ListView):  ## Show public presets
+    model = Preset
+    template_name = "public.html"
+    context_object_name = "presets"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        presets = Preset.objects.filter(private=False)
+        context['presets'] = presets
+        return context
+
+
+class ListPrivatePresetsView(ListView): ## Show private presets
+    model = Preset
+    template_name = "main.html"
+    context_object_name = "presets"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        presets = Preset.objects.filter(author=self.request.user)
+        context['presets'] = presets
+        return context
+
+class ListPresetsView(ListView): ## Adaptive
+    model = Preset
+    template_name = "main.html"
+    context_object_name = "presets"
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if self.request.user.is_anonymous:
+            presets = Preset.objects.filter(private=False)
+        else:
+            presets = Preset.objects.filter(Q(author=self.request.user) | Q(private=False))
+        context['presets'] = presets
+        return context
+
 
 
 class DetailPresetView(DetailView):
