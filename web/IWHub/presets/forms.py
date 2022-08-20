caaa8648b0c7a4eeeca73e3766cd098c04b8c0a8
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2022-08-20 22:07:12 +0300

    fix: private flag

--- web/IWHub/presets/forms.py
+++ web/IWHub/presets/forms.py
@@ -6,4 +6,4 @@ class UploadPresetForm(forms.Form):
     title = forms.CharField(max_length=255, label="Название")
     description = forms.CharField(max_length=512, widget=forms.Textarea, label="Описание")
     widget_set = forms.CharField(max_length=255, label="Набор виджетов")
-    private = forms.BooleanField(label="Приватный", initial=True)
+    private = forms.BooleanField(label="Приватный", initial=True, required=False,)
