ca34814e0f2c28c15548de9d0cf3919d80368ce7
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2022-08-20 22:08:40 +0300

    fix: settings.py
    changing the storage location

--- web/IWHub/IWHub/settings.py
+++ web/IWHub/IWHub/settings.py
@@ -121,9 +121,12 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'static'
+    BASE_DIR / 'static',
+    BASE_DIR / 'storage',
 ]
 
+MEDIA_URL = '/storage/'
+MEDIA_ROOT = BASE_DIR / "storage/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
