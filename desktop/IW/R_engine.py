d4e9fbfed7e3aae46ee022d92a0c89ec04f73810
Alexandr Pushylo <alexandr.pushylo@gmail.com>
2022-09-04 20:28:15 +0300

    read settings from file

--- desktop/IW/R_engine.py
+++ desktop/IW/R_engine.py
@@ -7,8 +7,8 @@ from PIL import Image, ImageDraw, ImageFont, ImageChops
 
 
 #----------connecting preset-------------------------
-# from preset_digital_clock import Preset
-# from preset_simple_date import Preset
+# from preset_digital_clock import DigitalClock as Preset
+# from preset_simple_date import SimpleDate as Preset
 
 from preset_complex_date import ComplexDate as Preset
 
@@ -18,35 +18,64 @@ from preset_complex_date import ComplexDate as Preset
 TETS_DIR = f"D:{sep}Temp{sep}t{sep}"
 
 class REngine():
-    preset_json_file = 'preset.json'
+    SETTINGS = 'settings.json'
+    file_preview_name = 'sys_wall.jpeg' #self.preset_conf['file_preview'] = "sys_wall.jpeg"
+    preset_json_file = 'preset.json'    #self.settings['preset_config_file'] = "preset.json"
     work_img_name = 'canvas'
-    work_img_path = f'{TETS_DIR}'
+    work_img_path = f'{TETS_DIR}'#######    #self.settings['workdir'] = "workdir"
     format_img = 'jpeg'
     widget_set = []
     Preset = Preset
 
     def __init__(self) -> None:
+        self.read_settings()
         self._buffer = ctypes.create_unicode_buffer(360)
         self._byte_buffer = io.BytesIO()
         
         self.original_image_path = ''
         self.canvas = None
-        self.canvas_path = f"{__class__.work_img_path}{__class__.work_img_name}"
+        self.canvas_path =f"{path.dirname(__file__)}{sep}{self.settings.get('workdir', __class__.work_img_path)}{sep}{__class__.work_img_name}"
+        
+        # self.settings.get('workdir', __class__.work_img_path) + __class__.work_img_name
+        
         
         self.get_wall_to_buffer()
         self.push_img()
         self.init_preset()
         self.dump_preset()
 
+
+    def read_settings(self):
+        #read setting.json
+        try:
+            with open(__class__.SETTINGS, 'r') as fp:
+                self.settings = json.load(fp)
+        except Exception as e:
+            print(e)
+        #read preset.json    
+        try:
+            preset_conf_name = self.settings.get('preset_config_file', __class__.preset_json_file)
+            with open(preset_conf_name, 'r') as fp:
+                self.preset_conf = json.load(fp)
+        except Exception as e:
+            print(e)
+        
+    
+    
+    def install_preset(self):
+        pass
+    
+    
     def dump_preset(self):
         preset = {}
         preset['name'] = self.preset.name
         preset['widgets'] = self.preset.type
         preset['module'] = self.preset.__module__
+        preset['file_preview'] = __class__.file_preview_name
         with open(__class__.preset_json_file, 'w') as fp:
             json.dump(preset, fp)
         self.clean_canvas()
-        self.preset.draw(self.canvas,'sys_wall.jpeg')
+        self.preset.draw(self.canvas, __class__.file_preview_name)
         
 
     def get_wall_to_buffer(self):
